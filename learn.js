05d49a5eeadde0cc0d2e84576e1126da1d7d4a6c
HimaniDas <himani.das1998@gmail.com>
2025-03-07 01:31:16 +0600

    newman updated with learn

---
+++ learn.js
@@ -0,0 +1,7 @@
+//function signature- eta diye function er parameter bujhai, function body- body r vitore sob kaj hoi
+
+function myInformation(name,age){
+    console.log("my name is",name,"my age is",age);
+
+}
+myInformation("Mithun", 30);
